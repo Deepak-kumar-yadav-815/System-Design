fb0289fca1af05de63f7db3749a0374193b9575e
Deepak kumar yadav <deepakkyadav815@gmail.com>
2025-12-19 20:30:49 +0530

    notifyfication halt

---
+++ 11_Notification_system_Design/notify.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class INotification {
+    public:
+        virtual string getContent()=0;
+        virtual ~INotification(){}
+};
+
+
+
+signed main() {
+    freopen("output.txt","w",stdout);
+
+}
